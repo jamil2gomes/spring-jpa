74417c71d55034cb567160425385c4f8d847a421
jamil2gomes <jamil.lannister23@gmail.com>
2019-09-24 10:45:08 -0300

    criação do findByName

--- src/main/java/com/jamil/springJpa/repositories/UserRepository.java
+++ src/main/java/com/jamil/springJpa/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.jamil.springJpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jamil.springJpa.domain.User;
 
-public interface UserRepository extends JpaRepository<User,Long > {
+public interface UserRepository extends JpaRepository<User,Long >{
 
+	List<User>findByName(String nome);
 }
